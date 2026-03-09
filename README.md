daa5f906dc5010f9661503d3444a884854752865
Emilio Sala <emilio_k88@hotmail.com>
2026-03-09 07:32:37 +0100

    ADD: hit server count in /metrics and /reset

--- README.md
+++ README.md
@@ -3,11 +3,13 @@ http://localhost:8080
 
 
 
-An http.Handler is just an interface:
+An http.Handler is just an interface
 
 ```go
 type Handler interface {
 	ServeHTTP(ResponseWriter, *Request)
 }
+
+type HandlerFunc func(ResponseWriter, *Request)
 ```
-Any type with a ServeHTTP method that matches the http.HandlerFunc signature above is an http.Handler. Take a second to think about it: it makes a lot of sense! To handle an incoming HTTP request, all a function needs is a way to write a response and the request itself.
+Any type with a ServeHTTP method that matches the http.HandlerFunc signature above is an http.Handler To handle an incoming HTTP request, all a function needs is a way to write a response and the request itself.
--- main.go
+++ main.go
@@ -1,16 +1,45 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
 
 func main(){
 
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	apiCfg := apiConfig{}
+
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
 	})
+
+	mux.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(fmt.Sprintf("Hits: %d", apiCfg.fileserverHits.Load())))
+	})
+
+	mux.HandleFunc("/reset", func(w http.ResponseWriter, r *http.Request) {
+		apiCfg.fileserverHits.Store(0)
+		w.WriteHeader(http.StatusOK)
+	})
+
 	server := http.Server{
 		Addr:    ":8080",
 		Handler: mux,
