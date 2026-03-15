492343edec882b105368fe8b55ca8152afa9f3e8
Emilio Sala <emilio_k88@hotmail.com>
2026-03-15 10:44:38 +0100

    ADD: create chirps now get token from header and validates

--- handleChirps.go
+++ handleChirps.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/Siddharta314/chirpygo/internal/auth"
 	"github.com/Siddharta314/chirpygo/internal/database"
 	"github.com/google/uuid"
 )
@@ -21,25 +22,34 @@ type Chirp struct {
 func (apiCfg *apiConfig) createChirp(w http.ResponseWriter, r *http.Request) {
 	type parameters struct{
 		Body string `json:"body"`
-		UserID uuid.UUID `json:"user_id"`
 	}
 	decoder := json.NewDecoder(r.Body)
 	var params parameters
 	err := decoder.Decode(&params)
 	if err != nil {
-		http.Error(w, "Error parsing request body", http.StatusInternalServerError)
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
 		return
 	}
 	if len(params.Body) > 140 {
 		respondWithError(w, http.StatusBadRequest, "Chirp is too long")
 		return
 	}
+	token, err := auth.GetBearerToken(r.Header)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Unauthorized")
+		return
+	}
+	uuid, err := auth.ValidateJWT(token, apiCfg.jwtSecret)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Unauthorized")
+		return
+	}
 	chirp, err := apiCfg.db.CreateChirp(r.Context(), database.CreateChirpParams{
 		Body:   params.Body,
-		UserID: params.UserID,
+		UserID: uuid,
 	})
 	if err != nil {
-		http.Error(w, "Error creating user", http.StatusInternalServerError)
+		respondWithError(w, http.StatusInternalServerError, "Couldn't create chirp")
 		return
 	}
 	respondWithJSON(w, 201, Chirp{
